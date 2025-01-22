6a94e59bc90f8da1cd4437ec5772756d63e975a8
Kunal <kunalcode05@gmail.com>
2025-01-23 02:19:03 +0530

    Added parsing header

--- main.c
+++ main.c
@@ -1,20 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include "display.h"
-
-//* Input parsing part
-void parseInput(const char *input, char *cell, char *expression) {
-    const char *equalSign = strchr(input, '=');
-    if (equalSign) {
-        strncpy(cell, input, equalSign - input);
-        cell[equalSign - input] = '\0';  // Null-terminate
-        strcpy(expression, equalSign + 1);
-    } else {
-        cell[0] = '\0';  // Indicate error
-        expression[0] = '\0';
-    }
-}
 
 int main(){
 
