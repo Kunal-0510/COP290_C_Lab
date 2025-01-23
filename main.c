3f898862e0ef417dedcb586ae3dc46bda449076e
Kunal <kunalcode05@gmail.com>
2025-01-24 01:35:12 +0530

    Completed Input parsing and cell validity

--- main.c
+++ main.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 
-int main(){
+int main() {
+    char input[100];
+    printf("Enter input : ");
+    scanf("%s", input);
 
+    char* cell = NULL;
+    char* expression = NULL;
+
+    if (parseInput(input, &cell, &expression)) {
+        printf("Cell: %s\n", cell);
+        printf("Expression: %s\n", expression);
+
+        int* index = giveIndex(cell);
+        if (index) {
+            printf("Row: %d, Column: %d\n", index[0], index[1]);
+            free(index);
+        }
+    } else {
+        printf("Invalid Command\n");
+    }
+
+    free(cell);
+    free(expression);
 
     return 0;
 }
