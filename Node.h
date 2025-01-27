5cc762c4c147269188cc293333a658fb61d6fca5
ShreyashMohanty2301 <es1221797@iitd.ac.in>
2025-01-27 19:10:06 +0000

    Added struct

---
+++ Node.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+struct Node{
+
+    int val = 0;
+    LinkedList *InNeighbours = Null;
+    int type = 1;
+    int *cell1 = NULL;
+    int *cell2 = NULL;
+    int *op_val = NULL;
+
+};
