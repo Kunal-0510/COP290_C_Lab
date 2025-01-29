d0dd68d1d616a544996237642e2fee92ca3b3bb1
ShreyashMohanty2301 <es1221797@iitd.ac.in>
2025-01-29 16:56:34 +0000

    Cycle Detection is done

--- Node.h
+++ Node.h
@@ -5,27 +5,19 @@
 #include "linkedlist.h"
 
 typedef struct Node{
-<<<<<<< HEAD
 
     int val ;
     LinkedList *InNeighbours;
+    LinkedList *OutNeighbours;
     int type;
     int *cell1;
     int *cell2;
     int *op_val;
-
+    int in_size;
+    
 }Node;
-=======
->>>>>>> c00332c06cb6f7cf8f8dd89aed1239dbec2e456b
 
-    int val ;
-    LinkedList *InNeighbours;
-    int type;
-    int *cell1;
-    int *cell2;
-    int *op_val;
 
-}Node;
 
 void initialize_node(struct Node *node);
 
