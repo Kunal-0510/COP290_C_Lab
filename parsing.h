efe4c77c8fc2c9f3d9745a3b774d811cc5d3ef3b
Kunal <kunalcode05@gmail.com>
2025-02-04 21:39:56 +0530

    minor changes in parsing.h

--- parsing.h
+++ parsing.h
@@ -11,4 +11,10 @@ bool parse(char* input, char** first, char** second, char symbol);
 
 void parseExpr(char* expression, char* op, char* val1, char* val2);
 
+Node* str_to_node(char* cellAddress,Sheet* sheet);
+
+bool assign_cell(char* cellAddress,char* expr,Sheet* sheet);
+
+bool parseInput(char* input,Sheet* sheet);
+
 #endif
